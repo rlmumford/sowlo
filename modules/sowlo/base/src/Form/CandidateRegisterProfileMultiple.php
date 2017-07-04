bbca99194919ed806bde38d98483ff229bd3a336
CounselKit Support <support@counselkit.com>
2017-07-04 08:55:35 +0000

    by rlmumford: Working towards a summary table.

--- modules/sowlo/base/src/Form/CandidateRegisterProfileMultiple.php
+++ modules/sowlo/base/src/Form/CandidateRegisterProfileMultiple.php
@@ -5,6 +5,7 @@ namespace Drupal\sowlo_base\Form;
 use Drupal\Component\Utility\Random;
 use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityManagerInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
@@ -36,6 +37,93 @@ abstract class CandidateRegisterProfileMultiple extends CandidateRegisterProfile
     $this->setEntity($cached_values['candidate_'.$step][$profile_indicator]);
 
     ContentEntityForm::init($form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildForm($form, $form_state);
+
+    // Build a summary table of profiles relevant to this step.
+    $cached_values = &$form_state->getTemporaryValue('wizard');
+    $step = $this->getProfileStep();
+    $profile_indicator = $cached_values["candidate_current_{$step}_profile"];
+    $form['profiles_summary'] = $this->buildProfileSummaryTable($cached_values["candidate_{$step}"], $profile_indicator);
+
+    return $form;
+  }
+
+  /**
+   * Build a summary table of other profiles of this type.
+   *
+   * @param Profile[] $profiles
+   * @param string $current
+   *
+   * @return array
+   */
+  public function buildProfileSummaryTable($profiles, $current) {
+    $build = array(
+      '#type' => 'table',
+      '#header' => $this->buildProfileSummaryTableHeader(),
+      '#title' => $this->getProfileSummaryTableTitle(),
+      '#rows' => [],
+      '#empty' => $this->t('There are no items yet.'),
+    );
+
+    foreach ($profiles as $key => $profile) {
+      if ($row = $this->buildProfileSummaryTableRow($profile, $key, ($current == $key))) {
+        $build['#rows'][$key] = $row;
+      }
+    }
+
+    return $build;
+  }
+
+  /**
+   * Build the table headers for the summary table.
+   */
+  public function buildProfileSummaryTableHeader() {
+    $row['operations'] = $this->t('Operations');
+    return $row;
+  }
+
+  /**
+   * Get the title for the table.
+   */
+  public function getProfileSummaryTableTitle() {
+    return $this->t('Other @step', ['@step' => ucfirst($this->getProfileStep())]);
+  }
+
+  /**
+   * Build a table row.
+   */
+  public function buildProfileSummaryTableRow(EntityInterface $profile, $indicator, $is_current = FALSE) {
+    $row = [];
+    $row['operations']['data'] = [
+      '#type' => 'container',
+      'edit' => [
+        '#type' => 'submit',
+        '#value' => $this->t('Edit'),
+        '#indicator' => $indicator,
+        '#submit' => array(
+          [$this, 'submitFormSwitchProfile'],
+        ),
+        '#validate' => [],
+        '#access' => !$is_current,
+      ],
+      'remove' => [
+        '#type' => 'submit',
+        '#value' => $this->t('Remove'),
+        '#indicator' => $indicator,
+        '#submit' => [
+          [$this, 'submitFormRemoveProfile'],
+        ],
+        '#validate' => [],
+        '#access' => !$is_current,
+      ],
+    ];
+    return $row;
   }
 
   /**
--- modules/sowlo/base/src/Form/CandidateRegisterWork.php
+++ modules/sowlo/base/src/Form/CandidateRegisterWork.php
@@ -29,4 +29,45 @@ class CandidateRegisterWork extends CandidateRegisterProfileMultiple {
   public function getProfileType() {
     return 'experience_work';
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildProfileSummaryTableHeader() {
+    $row = [
+      'job' => $this->t('Job'),
+      'date' => $this->t('Date'),
+    ];
+    $row += parent::buildProfileSummaryTableHeader();
+    return $row;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildProfileSummaryTableRow($profile, $indicator, $is_current = FALSE) {
+    if ($is_current) {
+      return FALSE;
+    }
+
+    $row['job']['data'] = $this->t(
+      '@job at @company',
+      [
+        '@job' => $profile->workexp_job_title->value,
+        '@company' => 'Company',
+      ]
+    );
+
+    $row['date']['data'] = $this->t(
+      '@start to @end',
+      [
+        '@start' => 'Now',
+        '@end' => 'Now',
+      ]
+    );
+    dpm($profile);
+
+    $row += parent::buildProfileSummaryTableRow($profile, $indicator, $is_current);
+    return $row;
+  }
 }
