412511b746522da840299056bf0261e9039cd833
rlmumford <rlmumford@googlemail.com>
2017-10-16 15:23:43 +0000

    by rlmumford: Updated Test.

--- modules/sowlo/base/test/script.php
+++ modules/sowlo/base/test/script.php
@@ -17,7 +17,7 @@ try {
           'country_code' => 'GB',
           'address_line1' => '4 Dale Street',
           'locality' => 'Manchester',
-          'postal code' => 'M22 7HJ',
+          'postal_code' => 'M22 7HJ',
         ),
       ),
     ),
@@ -48,6 +48,47 @@ try {
             'qualification_grade' => 'A',
             'qualification_level' => 'gcse',
             'qualification_title' => 'English Literature',
+          ),
+        ),
+      ),
+    ),
+    'experience_work' => array(
+      array(
+        'workexp_company' => 'Websites Inc.',
+        'workexp_current' => FALSE,
+        'workexp_start' => '2011-11-20',
+        'workexp_end' => '2013-09-01',
+        'workexp_job_title' => 'Junior Developer',
+        'workexp_location' => array(
+          'type' => 'address',
+          'address' => 'country_code',
+          'address_line1' => '123 A Street',
+          'locality' => 'A Town',
+          'postal_code' => 'M1 5JG',
+        ),
+        'workexp_projects' => array(
+          array(
+            'proj_company' => 'Twitter Inc.',
+            'proj_description' => 'Working on a plugin for Twitter',
+            'proj_from' => '2011-11-20',
+            'proj_to' => '2012-02-21',
+            'proj_name' => 'Random Twitter Plugin',
+            'proj_responsibilities' => array(
+              array(
+                'resp_responsibility' => 'Scripting PHP',
+                'resp_description' => 'Wrote High Quality PHP Code to make the plugin work',
+              ),
+              array(
+                'resp_responsibility' => 'Code QA',
+                'resp_description' => 'Tested and fixed code and was responsible for quality assurance',
+              ),
+            ),
+            'proj_skills' => array(
+              array(
+                'skill_skill' => 'PHP',
+                'skill_level' => 'expert',
+              ),
+            ),
           ),
         ),
       ),
